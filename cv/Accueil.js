9d49f1c3635e5112a6c073ebebd4ed98f9e006a9
Filps-13 <ruitaneia@gmail.com>
2026-01-09 13:32:29 +0100

    Add files via upload

---
+++ cv/Accueil.js
@@ -0,0 +1,11 @@
+const mainTitle = document.querySelector("h1")
+
+console.log(mainTitle);
+
+const cards = document.querySelectorAll(".card")
+
+console.log(cards)
+
+cards.forEach(function (card)  {
+    card.querySelector("p")
+})
